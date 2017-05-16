eeea61bf2ca517d5bac77433a8ae694d5370e564
jean <jeantovar@gmail.com>
2017-05-16 18:01:19 -0400

    add Devise and Styling

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,8 @@
 class WelcomeController < ApplicationController
+
+	#con esta instruccion evito el sistema de autenticacion de Devise
+	skip_before_action :authenticate_user!, only: [:index]
+	
   def index
   end
 end
