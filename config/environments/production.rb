7195621f17a53dce184a44a061cecac423c9f02c
jean <jeantovar@gmail.com>
2017-08-06 12:44:49 -0400

    basic SendGrid configuration added

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,6 +52,11 @@ Rails.application.configure do
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
+  #Sendgrid configiration
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.default_url_options = {:host => 'photo-app-j34n.herokuapp.com/', :protocol => 'https'}
+
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "photo-app_#{Rails.env}"
